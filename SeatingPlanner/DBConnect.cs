30fcdbd25c4cdef2dd098a8d0bca6a8abdbb45b7
DhruvishaSupeda <dhruvishasupeda@gmail.com>
2019-07-23 15:12:37 +0100

    Made stuff more usable

--- SeatingPlanner/DBConnect.cs
+++ SeatingPlanner/DBConnect.cs
@@ -264,6 +264,31 @@ namespace SeatingPlanner
             }
         }
 
+        public List<Student> convertStudents()
+        {
+            //gets number of rows/records
+            int length = SelectID().Count;
+            //Gets all of the rows and the data in the rows
+            List<string>[] allRows = SelectByRow();
+            List<Student> list = new List<Student>();
+            //Makes student for each row then adds to list of students
+            for (int i = 0; i < length; i++)
+            {
+                List<string> selected = allRows[i];
+                string fore = selected[0];
+                string sur = selected[1];
+                string gend = selected[2];
+                int index = selected[3].IndexOf(' ');
+                string doobb = selected[3].Substring(0, index);
+                int windooww = int.Parse(selected[4]);
+                int doorr = int.Parse(selected[5]);
+                int froont = int.Parse(selected[6]);
+                Student student = new Student(fore, sur, gend, doobb, windooww, doorr, froont);
+                list.Add(student);
+            }
+            return list;
+        }
+
         //Count statement
         //public int Count()
         //{
--- SeatingPlanner/ViewStudentsWindow.xaml.cs
+++ SeatingPlanner/ViewStudentsWindow.xaml.cs
@@ -22,7 +22,6 @@ namespace SeatingPlanner
     /// </summary>
     public partial class ViewStudentsWindow : Window
     {
-        private BindingSource bindingSource1 = new BindingSource();
         DBConnect db = new DBConnect();
 
         public ViewStudentsWindow()
@@ -33,26 +32,7 @@ namespace SeatingPlanner
             //seems the easiest way idk
 
             //gets array of lists for everything (which is db.Select())
-            //gets number of rows/records
-            int length = db.SelectID().Count;
-            //Gets all of the rows and the data in the rows
-            List<string>[] allRows = db.SelectByRow();
-            List<Student> list = new List<Student>();
-            //Makes student for each row then adds to list of students
-            for (int i = 0; i < length; i++)
-            {
-                List<string> selected = allRows[i];
-                string fore = selected[0];
-                string sur = selected[1];
-                string gend = selected[2];
-                int index = selected[3].IndexOf(' ');
-                string doobb = selected[3].Substring(0, index);
-                int windooww = int.Parse(selected[4]);
-                int doorr = int.Parse(selected[5]);
-                int froont = int.Parse(selected[6]);
-                Student student = new Student(fore, sur, gend, doobb, windooww, doorr, froont);
-                list.Add(student);
-            }
+            List<Student> list = db.convertStudents();
             dtgStudents.ItemsSource = list;
         }
     }
