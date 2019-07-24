54ddafad620285ad5bebf76d297f64a06e593216
DhruvishaSupeda <dhruvishasupeda@gmail.com>
2019-07-24 18:17:02 +0100

    added basic sorting stuff using OrderBy

--- SeatingPlanner/SortStudentWindow.xaml.cs
+++ SeatingPlanner/SortStudentWindow.xaml.cs
@@ -17,8 +17,10 @@ namespace SeatingPlanner
     /// <summary>
     /// Interaction logic for Window2.xaml
     /// </summary>
-    public partial class SortStudentWindow : Window
-    {
+    public partial class SortStudentWindow : Window { 
+
+        DBConnect db = new DBConnect();
+        
         public SortStudentWindow()
         {
             InitializeComponent();
@@ -29,5 +31,54 @@ namespace SeatingPlanner
             new MainWindow().Show();
             this.Close();
         }
+
+        private void BtnSort_Click(object sender, RoutedEventArgs e)
+        {
+            //Just sorting them into ascending grade for now
+            Alphabetical();
+        }
+
+        private void AscGrade()
+        {
+            List<Student> list = db.convertStudents();
+            int length = list.Count;
+            List<Student> SortedList = list.OrderBy(o => o.average_grade).ToList();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(SortedList[i].forename);
+                Console.WriteLine(" " + SortedList[i].average_grade);
+            }
+        }
+
+        private void DescGrade()
+        {
+            List<Student> list = db.convertStudents();
+            int length = list.Count;
+            List<Student> SortedList = list.OrderByDescending(o => o.average_grade).ToList();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(SortedList[i].forename);
+                Console.WriteLine(" " + SortedList[i].average_grade);
+            }
+        }
+
+        private void Alphabetical()
+        {
+            List<Student> list = db.convertStudents();
+            int length = list.Count;
+            List<Student> SortedList = list.OrderBy(o => o.surname).ToList();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(SortedList[i].surname);
+                //Console.WriteLine(" " + SortedList[i].average_grade);
+            }
+        }
+
+        private void Gender()
+        {
+            
+        }
+
+
     }
 }
